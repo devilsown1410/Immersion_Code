b897c009321639041ff9ef5a68f8a28274a5481d
devilsown1410 <122162757+devilsown1410@users.noreply.github.com>
2024-06-25 21:04:05 +0530

    Assignment Part 1

--- ClassCodes/maximumdepth.java
+++ ClassCodes/maximumdepth.java
@@ -1,14 +1,6 @@
 package ClassCodes;
 
-import javax.swing.tree.TreeNode;
-
 public class maximumdepth {
-    public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
-    }
     public class TreeNode {
       int val;
       TreeNode left;
@@ -20,5 +12,14 @@ public class maximumdepth {
           this.left = left;
           this.right = right;
       }
- }
+    }
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+    public static void main(String[] args) {
+        
+    }
 }
